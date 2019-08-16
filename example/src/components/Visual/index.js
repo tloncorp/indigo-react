f881e920a0fd8eeceb6ea630ed9687c0547866db
Nick Beattie <nick@nickbytes.com>
2019-08-15 22:38:35 -0700

    feature: styling and construction of inputs
    feature: input styling and construction
    
    cleanup

--- example/src/components/Visual/index.js
+++ example/src/components/Visual/index.js
@@ -1,11 +1,96 @@
 import React from "react";
-import { P, Div, H2 } from "indigo-react";
+import {
+  Div,
+  H2,
+  FieldError,
+  FieldInput,
+  FieldSuccess,
+  Span
+} from "indigo-react";
 
 const Visual = () => (
-  <Div maxWidth="800px">
-    <H2>Visual</H2>
-    <P>This is the Visual section</P>
-  </Div>
+  <React.Fragment>
+    <Div maxWidth="800px">
+      <H2>Basic Inputs</H2>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput placeholder="Placeholder" name="Email" />
+        {false && <FieldError fontSize={[1]}>Error example</FieldError>}
+      </Div>
+
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput placeholder="Placeholder" name="Email" hasError />
+        {true && <FieldError fontSize={[1]}>Error example</FieldError>}
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput placeholder="Placeholder" name="Email" hasSuccess />
+        {true && <FieldSuccess fontSize={[1]}>Success!</FieldSuccess>}
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]} color="gray.2">
+          Label
+        </Span>
+        <FieldInput disabled placeholder="Placeholder" name="Email" />
+        {false && <FieldSuccess fontSize={[1]}>Success!</FieldSuccess>}
+      </Div>
+    </Div>
+    <Div mt={[8]} maxWidth="800px">
+      <H2>Search Inputs</H2>
+      <Div my={[3]} maxWidth={[12]}>
+        <FieldInput placeholder="Search" name="Search" />
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <FieldInput placeholder="Search" name="Search" hasError />
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <FieldInput placeholder="Search" name="Search" disabled />
+      </Div>
+    </Div>
+
+    <Div mt={[8]} maxWidth="800px">
+      <H2>Textarea</H2>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput
+          as="textarea"
+          placeholder="Distributed protocol layer gaming world order"
+          name="textarea"
+        />
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput
+          as="textarea"
+          placeholder="Distributed protocol layer gaming world order"
+          name="textarea"
+          hasError
+        />
+      </Div>
+      <Div my={[3]} maxWidth={[12]}>
+        <Span as="label" htmlFor="Email" fontSize={[1]}>
+          Label
+        </Span>
+        <FieldInput
+          as="textarea"
+          placeholder="Distributed protocol layer gaming world order"
+          name="textarea"
+          disabled
+        />
+      </Div>
+    </Div>
+  </React.Fragment>
 );
 
 export default Visual;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 // Primitives
-import Box from './components/primitives/Box'
-import P from './components/primitives/P'
-import Span from './components/primitives/Span'
-import Div from './components/primitives/Div'
-import Flex from './components/primitives/Flex'
-import Img from './components/primitives/Img'
-import { Ul, Ol, Li } from './components/primitives/Lists'
-import { H1, H2, H3, H4, H5, H6 } from './components/primitives/StyledH'
+import Box from "./components/primitives/Box";
+import P from "./components/primitives/P";
+import Span from "./components/primitives/Span";
+import Div from "./components/primitives/Div";
+import Flex from "./components/primitives/Flex";
+import Img from "./components/primitives/Img";
+import { Ul, Ol, Li } from "./components/primitives/Lists";
+import { H1, H2, H3, H4, H5, H6 } from "./components/primitives/StyledH";
+import {
+  FieldSuccess,
+  FieldError,
+  FieldInput
+} from "./components/primitives/FieldInput";
 // Theme (dev purposes)
-import theme from './theme'
+import theme from "./theme";
 
-import StyledA from './components/primitives/StyledA'
-import StyledLink from './components/primitives/StyledLink'
-import StyledInput from './components/primitives/StyledInput'
+import StyledA from "./components/primitives/StyledA";
+import StyledLink from "./components/primitives/StyledLink";
 
 export {
   P,
@@ -31,7 +35,9 @@ export {
   H5,
   H6,
   theme,
+  FieldSuccess,
+  FieldError,
+  FieldInput,
   StyledA,
-  StyledLink,
-  StyledInput
-}
+  StyledLink
+};
