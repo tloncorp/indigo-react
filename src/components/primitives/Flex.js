a67057efa086f6d4fe01c6aea26fbbbd863e80c9
Nick Beattie <nick@nickbytes.com>
2019-09-10 23:21:49 -0700

    cleanup: remove div, keep flex but remove wrap

--- src/components/primitives/Flex.js
+++ src/components/primitives/Flex.js
@@ -1,14 +1,13 @@
-import styled from 'styled-components'
-import { flexbox, space, typography, layout, compose } from 'styled-system'
+import styled from "styled-components";
+import { flexbox, space, typography, layout, compose } from "styled-system";
 
 const Flex = styled.div`
   display: flex;
-  flex-wrap: wrap;
   ${compose(
     flexbox,
     typography,
     space,
     layout
   )}
-`
-export default Flex
+`;
+export default Flex;
