43cef85c8e5b91fa215555cc77aeb01f95afecbb
Gavin Atkinson <gvnatknsn@protonmail.com>
2020-10-29 15:49:21 -0700

    fix spaces icon

--- src/Icon/iconIndex.tsx
+++ src/Icon/iconIndex.tsx
@@ -717,10 +717,26 @@ const Info = () => (
 
 const Spaces = () => (
   <>
-    <rect x="2.5" y="2.5" width="4" height="4" rx="0.5" />
-    <rect x="9.5" y="2.5" width="4" height="4" rx="0.5" />
-    <rect x="9.5" y="9.5" width="4" height="4" rx="0.5" />
-    <rect x="2.5" y="9.5" width="4" height="4" rx="0.5" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 3H3L3 6H6V3ZM3 2C2.44772 2 2 2.44772 2 3V6C2 6.55228 2.44772 7 3 7H6C6.55228 7 7 6.55228 7 6V3C7 2.44772 6.55228 2 6 2H3Z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13 3H10L10 6H13V3ZM10 2C9.44772 2 9 2.44772 9 3V6C9 6.55228 9.44772 7 10 7H13C13.5523 7 14 6.55228 14 6V3C14 2.44772 13.5523 2 13 2H10Z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13 10H10L10 13H13V10ZM10 9C9.44772 9 9 9.44772 9 10V13C9 13.5523 9.44772 14 10 14H13C13.5523 14 14 13.5523 14 13V10C14 9.44772 13.5523 9 13 9H10Z"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 10H3L3 13H6V10ZM3 9C2.44772 9 2 9.44772 2 10V13C2 13.5523 2.44772 14 3 14H6C6.55228 14 7 13.5523 7 13V10C7 9.44772 6.55228 9 6 9H3Z"
+    />
   </>
 );
 
