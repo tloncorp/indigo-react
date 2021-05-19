3ecca2ef21423033f4be8d4d7b69a4fd1b16abdf
Liam Fitzgerald <liam.fitzgerald@tlon.io>
2021-05-19 13:55:14 +1000

    Li: respect indigo font, color

--- src/Li.tsx
+++ src/Li.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
-import { compose } from "styled-system";
+import css, { SystemStyleObject } from "@styled-system/css";
 import { allSystemStyle, AllSystemProps } from "./system/unions";
 
 export type LiProps = AllSystemProps;
 
+const style = ({ color, fontFamily, fontSize }: LiProps) =>
+  css({
+    "&::marker": {
+      color: typeof color === "undefined" ? "black" : color,
+      fontFamily: typeof fontFamily === "undefined" ? "sans" : fontFamily,
+      fontSize: typeof fontSize === "undefined" ? 1 : fontSize
+    },
+  } as SystemStyleObject);
+
 export const Li = styled.li<React.PropsWithChildren<LiProps>>(
-  compose(...allSystemStyle)
+  style, ...allSystemStyle
 );
 
 Li.displayName = "Li";
